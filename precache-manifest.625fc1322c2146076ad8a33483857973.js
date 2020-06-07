76430b000213190c8d947ff3a23771aa7ecaf9dd
Madhav kabra <madhav.kabra1100@gmail.com>
2020-06-07 17:35:52 +0530

    Updates

---
+++ precache-manifest.625fc1322c2146076ad8a33483857973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a0b3bd2acfc8a7a66b8e79dc65e15a3",
+    "url": "/Notelist/index.html"
+  },
+  {
+    "revision": "9a6bb6f55f4c2857a925",
+    "url": "/Notelist/static/css/main.f59a9b27.chunk.css"
+  },
+  {
+    "revision": "7c7549a2cc66be1c0f2b",
+    "url": "/Notelist/static/js/2.bd149ac9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Notelist/static/js/2.bd149ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6bb6f55f4c2857a925",
+    "url": "/Notelist/static/js/main.f97c3dca.chunk.js"
+  },
+  {
+    "revision": "c355e101ec70cc074104",
+    "url": "/Notelist/static/js/runtime-main.ac9adeb9.js"
+  }
+]);
